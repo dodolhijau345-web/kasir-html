911c9059ee3f2672bebeb62b76270b504c9b7109
dodolhijau345-web <dodolhijau345@gmail.com>
2026-07-22 23:19:28 +0700

    Add files via upload

---
+++ sw-paul.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('laundry-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/paul-it.html',
+                '/icon-192.png',
+                '/icon-512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
